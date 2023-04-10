7b9fe59dc6e91d5aa3bf0356a17a3d3171c72505
Sarah Bock <mail.sarahbock@gmail.com>
2023-04-11 09:35:53 +1000

    image into divs

--- src/screens/Dhukarr.js
+++ src/screens/Dhukarr.js
@@ -64,13 +64,52 @@ const Dhukarr = () => {
           </map>   
           */}
       
-      <div className='contentContainer longImageHolder'>
+      <div className='contentContainer'>
+
+        <div className='introHolder padThin'>
+
+          <div className='padThinner'>
+            <h1>Yolŋu Yothuw Dhukarr ŋuthanaraw ga dharaŋanaraw. Ga nhaltjan gurruṯumirriy guŋga’yun dhu yothuny</h1>
+            <h2 className='marginTop'>The Yolŋu child’s developmental pathway and ways that family monitor and support the child’s development. </h2>
+            <h3 className='marginTop'>Colour key</h3>
+          </div>
+
+          <div className='blobRow'>
+            <div className='blob' id='intro1Holder'>
+              <div className='blobText blobBrown'>Ongoing ways that family support the child</div>
+            </div>
+            <div className='blob' id='intro2Holder'>
+              <div className='blobText blobOrange'>Ways that family support the child just at this stage</div>
+            </div>
+            <div className='blob' id='intro3Holder'>
+              <div className='blobText blobYellow'>What the child does - ongoing </div>
+            </div>
+            <div className='blob' id='intro4Holder'>
+              <div className='blobText blobLight'>What the child does – just at this stage </div>
+            </div>
+          </div>
+
+          <div className='blobRow'>
+            <div className='blob' id='intro5Holder'>
+              <div className='blobText blobGrey'>Yellow text – ways that family assess the child’s development</div>
+            </div>
+          </div>
+
+          <div className='padThinner'>
+            <p className='marginTop paddingBottom'>Yolŋu monitor and support the child’s development through watching and assessing how each child is progressing along their pathway and helping with what they need at the time – not expecting specific stages at specific ages like the Balanda system. </p>
+          </div>
+          
+          
+         
+
+
+        </div>
       
 
 
+         {/*
          <img className='longImage' src={language ? dhukarr_yolngu : dhukarr_english}/>
-
-      <div className="audioTrigger" id="audioTrigger3" style={{top: "1630px", left:"550px", width:"50px", height: "50px"}}>
+          <div className="audioTrigger" id="audioTrigger3" style={{top: "1630px", left:"550px", width:"50px", height: "50px"}}>
           <SoundPlayer source={require('../assets/mp3/recording3.mp3')}/>
         </div>
 
@@ -78,6 +117,10 @@ const Dhukarr = () => {
           <SoundPlayer source={require('../assets/mp3/recording4.mp3')}/>
         </div>
         
+         */
+         }
+
+     
         
         
       </div>
