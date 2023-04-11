d47121791970268f0fa71fca6769a14178b33e65
Sarah Bock <mail.sarahbock@gmail.com>
2023-04-11 17:00:49 +1000

    ongoing

--- src/screens/Dhukarr.js
+++ src/screens/Dhukarr.js
@@ -69,12 +69,12 @@ const Dhukarr = () => {
         <div className='introHolder padThin'>
 
           <div className='padThinner'>
-            <h1>Yolŋu Yothuw Dhukarr ŋuthanaraw ga dharaŋanaraw. Ga nhaltjan gurruṯumirriy guŋga’yun dhu yothuny</h1>
+            <h1>Yolŋu Yothuw Dhukarr ŋuthanaraw ga dharaŋanaraw. Ga nhaltjan dhu gurruṯumirriy guŋga’yun yothuny.</h1>
             <h2 className='marginTop'>The Yolŋu child’s developmental pathway and ways that family monitor and support the child’s development. </h2>
-            <h3 className='marginTop'>Colour key</h3>
+            <h3 className='marginTopLarge'>Colour Key</h3>
           </div>
 
-          <div className='blobRow'>
+          <div className='blobRow blobRowThin'>
             <div className='blob' id='intro1Holder'>
               <div className='blobText blobBrown'>Ongoing ways that family support the child</div>
             </div>
@@ -89,7 +89,7 @@ const Dhukarr = () => {
             </div>
           </div>
 
-          <div className='blobRow'>
+          <div className='blobRow blobRowThin'>
             <div className='blob' id='intro5Holder'>
               <div className='blobText blobGrey'>Yellow text – ways that family assess the child’s development</div>
             </div>
@@ -98,7 +98,62 @@ const Dhukarr = () => {
           <div className='padThinner'>
             <p className='marginTop paddingBottom'>Yolŋu monitor and support the child’s development through watching and assessing how each child is progressing along their pathway and helping with what they need at the time – not expecting specific stages at specific ages like the Balanda system. </p>
           </div>
+
+          {/*ONGOING*/}
+
+          <div className='blobRow'>
+
+            <div className='blob' id='blob8Holder'>
+              <div className='blobText blobBrown'>
+                <span className='englishText'>8In the Yolŋu system, we all work together to care and support the child’s development.</span>
+                <span className='yolnguText'>Dhuwal yothuw gakal yolŋuwal romŋur ŋanapurruŋgal djägany ŋuli räḻ-manapanmirr.  </span>
+              </div>
+            </div>
+
+            <div className='blob' id='blob3Holder'>
+              <div className='blobText blobYellow'>
+              <span className='englishText'>3The child knows what is best for their growth – when to sleep, eat, play (the child decides not the family). </span>
+                <span className='yolnguText'>Dhuwal yothuw gakal yolŋuwal romŋur ŋanapurruŋgal djägany ŋuli räḻ-manapanmirr  </span>
+              </div>
+            </div>
+
+            <div className='blob' id='blob5Holder'>
+              <div className='blobText blobYellow'>
+              <span className='englishText'>5Gurruṯu (kinship connection) testing—teaching—testing—teaching routines continue all the time until the child has demonstrated strong understanding of kinship concepts and their own relationship</span>
+                <span className='yolnguText'>Gurruṯuny ŋanapurr ŋuli marŋgi- gurrapan ŋunhi yothuny ŋayi ŋuli marrtji ŋuthan. Ga bulu ŋanapurr ŋuli walalany birrka’yun nhä ŋayi ŋunhi gurruṯu nhanŋu yothuw bitjan bili. </span>
+              </div>
+            </div>
+
+          </div>
           
+          <div className='blobRow'>
+
+            <div className='blob' id='blob7Holder'>
+              <div className='blobText blobBrown'>
+                <span className='englishText'>7Grandparents support and offer advice to mum and dad and rest of the family so they understand how to care for the child.
+</span>
+                <span className='yolnguText'>Märiy ga ŋathiy ŋuli guŋga’yun ga gurrupan dharaŋanaraw bäpaw ga ŋaṉḏiw  ga wiripuny mala gurruṯumirriw märr walal dhu dharaŋan nhaltjan walal dhu djäga yothuw.    </span>
+              </div>
+            </div>
+
+            <div className='blob' id='blob4Holder'>
+              <div className='blobText blobGrey'>
+              <span className='englishText'>4Family constantly observe and monitor how the child is developing in all areas (identity, skills, knowledge, character etc) - ‘watching what knowledge the child has caught’ (ref)
+ </span>
+                <span className='yolnguText'>Gurruṯumirriy ŋuli dhuwurr nhäma yothuny - nhaltjan ŋayi ŋuli romgu ga gakalwu marŋgithirr bitjan bili  </span>
+              </div>
+            </div>
+
+            <div className='blob' id='blob6Holder'>
+              <div className='blobText blobYellow'>
+              <span className='englishText'>6The child decides what they want or don’t want – they have autonomy – family don’t force them or hurt their feelings by refusing to give them what they want. 
+</span>
+                <span className='yolnguText'>Yothuw ŋayaŋu nhaku ŋayi djäl ga nhaku malaŋuw – ŋayi ŋunhi rumbal waṯaŋu – gurruṯumirriy ŋuli yaka gurkuryun wo ŋayaŋu wutthun yothuny  </span>
+              </div>
+            </div>
+
+            </div>
+
           
          
 
