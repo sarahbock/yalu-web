516ed0a4de70b60ce9cbb96d7e01fcc38f2464ca
Sarah Bock <mail.sarahbock@gmail.com>
2023-04-27 12:06:33 +1000

    remove plainenglish

--- src/screens/DictionaryEntry.js
+++ src/screens/DictionaryEntry.js
@@ -10,8 +10,8 @@ const DictionaryEntry = () => {
   const { state } = useLocation();
   const { item } = state;
   const totalPages =
-    (item?.yolngu3 || item?.yolngu3audio || item?.english3 || item?.english3audio || item?.plainenglish3 || item?.plainenglish3audio || item?.example3 || item?.example3audio) ? 3 :
-    (item?.yolngu2 || item?.yolngu2audio || item?.english2 || item?.english2audio || item?.plainenglish2 || item?.plainenglish2audio || item?.example2 || item?.example2audio) ? 2 :
+    (item?.yolngu3 || item?.yolngu3audio || item?.english3 || item?.english3audio || item?.example3 || item?.example3audio) ? 3 :
+    (item?.yolngu2 || item?.yolngu2audio || item?.english2 || item?.english2audio || item?.example2 || item?.example2audio) ? 2 :
     1;
 
   const [page, setPage] = useState(1);
@@ -19,7 +19,6 @@ const DictionaryEntry = () => {
   const [definition, setDefinition] = useState({
     yolngu: null,
     english: null,
-    plainenglish: null,
     example: null,
   });
   //console.log(item)
@@ -37,11 +36,6 @@ const DictionaryEntry = () => {
       item['english'+page+'audio'] ? 'Listen to the explanation in English:' : 'Nothing added yet',
 
       englishaudio: item['english'+page+'audio']?.src ?? null,
-
-      plainenglish: item['plainenglish'+page] ? item['plainenglish'+page] :
-      item['plainenglish'+page+'audio'] ? 'Listen to the explanation in plain English:' : 'Nothing added yet',
-
-      plainenglishaudio: item['plainenglish'+page+'audio']?.src ?? null,
 
       example: item['example'+page] ? item['example'+page] :
       item['example'+page+'audio'] ? 'Listen to an example:' : 'Nothing added yet',
@@ -84,20 +78,6 @@ const DictionaryEntry = () => {
     </div>
     <div className="entrySectionAudio">
       <SoundPlayer source={definition.englishaudio}/>
-    </div>
-  </div>
-</div>
-
-<div className="entryContent">
-  <div className="entryContentHeader">
-    Plain English
-  </div>
-  <div className="entrySection">
-    <div className="entrySectionText">
-      {definition.plainenglish}
-    </div>
-    <div className="entrySectionAudio">
-      <SoundPlayer source={definition.plainenglishaudio}/>
     </div>
   </div>
 </div>
