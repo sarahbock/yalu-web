dc8e1d793afa124d84070c724f1286d80fbb59a5
Sarah Bock <mail.sarahbock@gmail.com>
2023-05-10 09:53:27 +1000

    Ceremony

--- src/screens/Ceremony.js
+++ src/screens/Ceremony.js
@@ -10,13 +10,16 @@ const Ceremony = () => {
 
   
   return(
-    <div className="container ceremonyScreen">
 
-      <TopNav back title="Ceremony as a Yolŋu classroom"/>
+    <div className="container brown dhukarrScreen ceremonyScreen">
+
+    <TopNav back noBorder colour='brown' title="Ceremony as a Yolŋu classroom"/>
 
       <div className='contentContainer'>
         
-      <p>Coming soon</p>
+      <p>The skills and knowledge that Yolŋu children develop outside the school context are often invisible to those from other cultural backgrounds. </p>
+      <p>These videos demonstrate one of the rich learning environments – ceremony - in which Yolŋu children are immersed from birth. </p>
+      <p>The videos also share other cultural information that Yolŋu consider helpful for Balanda working in their community.</p>
       </div>
 
         
